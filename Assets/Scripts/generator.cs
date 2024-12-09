3d51fec37599f998cb8157a9fcbb91c27a78451a
New-Oleg <alexkokosov095@gmail.com>
2024-12-09 23:33:34 +0500

    add mov, destroy, boost movspead

--- Assets/Scripts/generator.cs
+++ Assets/Scripts/generator.cs
@@ -2,26 +2,29 @@ using UnityEngine;
 
 public class generator : MonoBehaviour
 {
+    // при переходе на большие префабы цифры нужно заменить
 
     public GameObject[] paterns;
-    
+    public Vector3 currentPosition; // позиция спавна
+
     void Start()
     {
-        generate(5); // стартавая генерация
-    }
-
-    void Update()
-    {
-        
+        currentPosition = transform.position - new Vector3(0,0,-7);
+        generate(6); // стартавая генерация
     }
 
     private void generate(int count){
-        Vector3 currentPosition = transform.position - new Vector3(0,1,-4);
-        
         for(int i =0; i<count;i++){
-        int number   =  Random.Range(0, 4);
+        int number   =  Random.Range(0, 4);  // выбор префаба
         Instantiate(paterns[number],currentPosition, Quaternion.identity);
-        currentPosition += Vector3.forward * 4;
+        currentPosition += Vector3.forward * 4;  // кмножение на длинну префаба
+        }
+    }
+
+    void OnCollisionEnter(Collision collision){ // для создания последующих препятствий
+        if(collision.collider.tag != "barer"){ 
+        currentPosition = new Vector3(0,0,14); // костыль (задаю корды спавна кждого следуюшего префаба, 2 длинны + половина) 
+        generate(1);
         }
     }
 }
---
+++ Assets/Scripts/mov.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class mov : MonoBehaviour
+{
+
+   public Transform target; 
+   public static float speed = 5f;
+   bool isbufspeed =false;
+
+    void Update()
+    {
+        if (target == null) return;
+
+        Vector3 direction = (target.position - transform.position).normalized; 
+        transform.position += direction * speed * Time.deltaTime; 
+
+        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        {
+            transform.position = target.position; 
+            isbufspeed = true;
+            Destroy(gameObject); 
+        }
+
+        if(isbufspeed ==true) bustMovSpead();
+    }
+
+    private void bustMovSpead(){ // ускорение после каждого пройденого префаба
+        speed +=0.1f;
+        isbufspeed =false;
+    }
+    
+}
