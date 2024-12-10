d2258559bddaf2632fb7ebab0d8a4aee7af8bbe1
New-Oleg <alexkokosov095@gmail.com>
2024-12-10 21:24:30 +0500

    add Dead and lower dodge

--- Assets/Scripts/dogeComponent.cs
+++ Assets/Scripts/dogeComponent.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class dogeComponent : MonoBehaviour
-{
+{   
     public float jumpForce = 1f;
 
     private Rigidbody rb;
@@ -28,6 +28,11 @@ public class dogeComponent : MonoBehaviour
 
         if(Input.GetButton("Jump") && isGrounded)
             rb.AddForce(new Vector3(0f, jumpForce * Time.deltaTime,0f), ForceMode.Impulse);
+
+        if(Input.GetKeyDown(KeyCode.S) && isGrounded){
+        transform.localScale = transform.localScale * 0.5f;
+        Invoke("ToBigger", 0.5f);
+        }
            
     }
 
@@ -35,6 +40,9 @@ public class dogeComponent : MonoBehaviour
     void OnCollisionEnter(Collision collision){
         if(collision.gameObject.CompareTag("ground"))
             isGrounded = true;
+
+        if(collision.gameObject.CompareTag("barer"))
+        Dead();
     }
     void OnCollisionExit(Collision collision){
         if(collision.gameObject.CompareTag("ground"))
@@ -42,4 +50,14 @@ public class dogeComponent : MonoBehaviour
     }
 
 
+
+    void Dead(){ // методж должен вызывать экран\меню смерти, паузу лучше прописать в Ui менеджере 
+        Destroy(gameObject);
+    }
+
+
+    void ToBigger(){
+        transform.localScale = transform.localScale * 2f;
+    }
+    // преследование в другом скрипте
 }
