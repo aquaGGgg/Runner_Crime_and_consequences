c3e2655c122acb146eb5c0ad138fd82456ec8296
New-Oleg <alexkokosov095@gmail.com>
2024-12-14 05:10:39 +0500

    add coin and coin counter

---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+
+public  class CoinCounter : MonoBehaviour
+{
+    public TextMeshProUGUI  text; 
+
+    private int _counter = 0;
+
+    void OnTriggerExit(Collider other){
+        if(other.gameObject.CompareTag("coin")){
+            _counter++;
+            Destroy(other.gameObject);
+            if(text != null)
+            text.text= "" + _counter;
+        }
+    }
+}
